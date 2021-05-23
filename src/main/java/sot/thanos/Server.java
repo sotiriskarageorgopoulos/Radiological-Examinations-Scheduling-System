2c8f2a4c820832bcfb51bd15e6f0ffd1cbc89a0a
sotiriskarageorgopoulos <sotiriskarageorgopoulos@gmail.com>
2021-05-23 14:09:03 +0300

    CORS issues solved!

--- src/main/java/sot/thanos/Server.java
+++ src/main/java/sot/thanos/Server.java
@@ -1,4 +1,5 @@
 package sot.thanos;
+
 import sot.thanos.controllers.*;
 
 import static spark.Spark.*;
@@ -17,25 +18,44 @@ public class Server {
 
         port(port);
 
-        path("/api/",() -> {
-            post("/register/:category",(req,res) -> RegisterController.register(req,res));
-            post("/login",(req,res) -> LoginController.login(req,res));
-            post("/insRadiologicalOrderDetails",(req,res) -> OrderSchedulingController.insRadiologicalOrderDetails(req,res));
-            post("/sendMessage", (req,res) -> ContactController.sendContactMsg(req,res));
-            get("/getRadiologicalOrders", (req,res) -> OrderSchedulingController.getRadiologicalOrders(req,res));
-            get("/getRadiologicalOrdersForRadiologist/:radiologistId",(req,res) -> RadiologistController.getRadiologicalOrdersForRadiologist(req,res));
-            get("/sortRadiologistsIdByAvailability",(req,res) -> RadiologistController.sortRadiologistsIdByAvailability(req,res));
-            get("/getRadiologistById/:radiologistId",(req,res) -> RadiologistController.getRadiologistById(req,res));
-            get("/getRadiologyOperations",(req,res) -> OrderSchedulingController.getRadiologyOperations(req,res));
-            get("/getHospitals",(req,res) -> HospitalController.getHospitals(req,res));
-            put("/scheduleRadiologyOrder/:patientCode", (req,res) -> OrderSchedulingController.scheduleRadiologyOrder(req,res));
-            delete("/deleteTheOldAppointments",(req,res) -> OrderSchedulingController.deleteTheOldAppointments(req,res));
+        path("/api/", () -> {
+            post("/register/:category", (req, res) -> RegisterController.register(req, res));
+            post("/login", (req, res) -> LoginController.login(req, res));
+            post("/insRadiologicalOrderDetails", (req, res) -> OrderSchedulingController.insRadiologicalOrderDetails(req, res));
+            post("/sendMessage", (req, res) -> ContactController.sendContactMsg(req, res));
+            get("/getRadiologicalOrders", (req, res) -> OrderSchedulingController.getRadiologicalOrders(req, res));
+            get("/getRadiologicalOrdersForRadiologist/:radiologistId", (req, res) -> RadiologistController.getRadiologicalOrdersForRadiologist(req, res));
+            get("/sortRadiologistsIdByAvailability", (req, res) -> RadiologistController.sortRadiologistsIdByAvailability(req, res));
+            get("/getRadiologistById/:radiologistId", (req, res) -> RadiologistController.getRadiologistById(req, res));
+            get("/getRadiologyOperations", (req, res) -> OrderSchedulingController.getRadiologyOperations(req, res));
+            get("/getHospitals", (req, res) -> HospitalController.getHospitals(req, res));
+            put("/scheduleRadiologyOrder/:patientCode", (req, res) -> OrderSchedulingController.scheduleRadiologyOrder(req, res));
+            delete("/deleteTheOldAppointments", (req, res) -> OrderSchedulingController.deleteTheOldAppointments(req, res));
         });
+
+        options("/*",
+                (request, response) -> {
+                    String accessControlRequestHeaders = request
+                            .headers("Access-Control-Request-Headers");
+                    if (accessControlRequestHeaders != null) {
+                        response.header("Access-Control-Allow-Headers",
+                                accessControlRequestHeaders);
+                    }
+
+                    String accessControlRequestMethod = request
+                            .headers("Access-Control-Request-Method");
+                    if (accessControlRequestMethod != null) {
+                        response.header("Access-Control-Allow-Methods",
+                                accessControlRequestMethod);
+                    }
+
+                    return "OK";
+                });
 
         before((req, res) -> {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Headers", "*");
-            res.header("Access-Control-Allow-Methods","DELETE, POST, GET, PUT, OPTIONS");
+            res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, PUT, OPTIONS");
             res.type("application/json");
         });
     }
